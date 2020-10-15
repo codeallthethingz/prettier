05d7f327e4f9d8e9d05dce183278884f8fba0928
Eric Hacke <eric@asserted.io>
2020-10-15 13:39:39 -0400

    feat: more details (#3)
    * feat: more details
    
    * fix: remove unnecessary tab width

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = {
-	"trailingComma": "es5",
 	"useTabs": true,
-	"semicolons": true,
-	"singleQuote": true
+	"printWidth": 150,
+	"semi": true,
+	"singleQuote": true,
+	"quoteProps": "as-needed",
+	"trailingComma": "es5",
+	"bracketSpacing": true,
+	"arrowParens": "always"
 }
